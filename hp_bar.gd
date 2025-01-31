e5439fe8bd2f6111b1f700f4d5f961876c02a85f
devtato <mstarzycki@gmail.com>
2025-01-30 21:47:40 -0800

    change to the heart icons for life

--- hp_bar.gd
+++ hp_bar.gd
@@ -3,24 +3,29 @@ extends HBoxContainer
 @onready var player = $"../../../../World/Player"
 @onready var container = $"."
 @onready var max_hp = player.max_life
-@onready var heart = $"../TextureRect2"
+@onready var heart = $"../Heart"
+@onready var full_heart_texture =  preload("res://assets/player/heart.png")
+@onready var depleated_heart_texture =  preload("res://assets/player/depleated_heart.png")
+
 var heart_list : Array[TextureRect]
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	for i in range(max_hp):
-		container.add_child(heart.duplicate())
-	heart_list.append_array(container.get_children())
-	pass # Replace with function body.
+    for i in range(max_hp):
+        var new_heart = heart.duplicate()
+        container.add_child(new_heart)
+        new_heart.visible = true
+    heart_list.append_array(container.get_children())
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+    pass
 
 
 func _on_player_take_dmg():
-	for i in range(heart_list.size()):
-		heart_list[i].visible = i < player.life
-	pass # Replace with function body.
+    print("redraw")
+    for i in range(player.life):
+        heart_list[i].set_texture(full_heart_texture)
+    for i in range (player.life, player.max_life):
+        heart_list[i].set_texture(depleated_heart_texture)
